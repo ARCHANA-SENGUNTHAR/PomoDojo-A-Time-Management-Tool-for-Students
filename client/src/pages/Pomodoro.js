2e89f51806c428504340a3c60b19533099a97278
ARCHANA-SENGUNTHAR <archanagurusamy648@gmail.com>
2026-02-14 12:31:51 +0530

    Fix axios import error

--- client/src/pages/Pomodoro.js
+++ client/src/pages/Pomodoro.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect, useRef, useCallback } from 'react';
 import './Pomodoro.css';
+import axios from "axios";
 import API from '../api/axios';
 
 const Pomodoro = () => {
